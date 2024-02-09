a747acb761c8080e9213b794cf7bbd0849d15d06
colemanw <coleman@civicrm.org>
2024-02-09 12:32:04 -0500

    Cleanup test class

--- tests/Command/ExtractCommandTest.php
+++ tests/Command/ExtractCommandTest.php
@@ -10,15 +10,18 @@ class ExtractCommandTest extends \PHPUnit\Framework\TestCase {
 
   protected $tmpFile = NULL;
 
-  public function examples() {
-    $cases = array(); // array(array $inputFiles, string $expectedOutputFile)
+  /**
+   * @return array{string $inputFiles, string $expectedOutputFile}[]
+   */
+  public function examples():array {
+    $cases = [];
 
-    $cases[] = array(array("examples/ex1.php"), "examples/ex1.pot");
-    $cases[] = array(array("examples/ex2.js"), "examples/ex2.pot");
-    $cases[] = array(array("examples/ex3.tpl"), "examples/ex3.pot");
-    $cases[] = array(array("examples/ex4.cmd", "examples/ex4.hlp", "examples/ex4.install", "examples/ex4.module", "examples/ex4.tpl", "examples/ex4.js", "examples/ex4.cmd2", "examples/ex4.txt"), "examples/ex4.pot");
-    $cases[] = array(array("examples/ex5.html"), "examples/ex5.pot");
-    $cases[] = array(array("examples/Event.setting.php"), "examples/event.setting.pot");
+    $cases[] = [["examples/ex1.php"], "examples/ex1.pot"];
+    $cases[] = [["examples/ex2.js"], "examples/ex2.pot"];
+    $cases[] = [["examples/ex3.tpl"], "examples/ex3.pot"];
+    $cases[] = [["examples/ex4.cmd", "examples/ex4.hlp", "examples/ex4.install", "examples/ex4.module", "examples/ex4.tpl", "examples/ex4.js", "examples/ex4.cmd2", "examples/ex4.txt"], "examples/ex4.pot"];
+    $cases[] = [["examples/ex5.html"], "examples/ex5.pot"];
+    $cases[] = [["examples/Event.setting.php"], "examples/event.setting.pot"];
 
     return $cases;
   }
@@ -39,10 +42,10 @@ class ExtractCommandTest extends \PHPUnit\Framework\TestCase {
   public function testExecute($inputFiles, $expectedOutputFile) {
     chdir($this->getBaseDir());
     $commandTester = $this->createCommandTester(new ExtractCommand());
-    $commandTester->execute(array(
+    $commandTester->execute([
       'command' => self::COMMAND,
       'files' => $inputFiles,
-    ));
+    ]);
     $expectedOutput = file_get_contents($expectedOutputFile);
     $this->assertEquals($expectedOutput, $commandTester->getDisplay());
   }
@@ -56,11 +59,11 @@ class ExtractCommandTest extends \PHPUnit\Framework\TestCase {
     chdir($this->getBaseDir());
     $this->tmpFile = tempnam(sys_get_temp_dir(), 'civistrings-');
     $commandTester = $this->createCommandTester(new ExtractCommand());
-    $commandTester->execute(array(
+    $commandTester->execute([
       'command' => self::COMMAND,
       'files' => $inputFiles,
       '--out' => $this->tmpFile,
-    ));
+    ]);
     $expectedOutput = file_get_contents($expectedOutputFile);
     $actualOutput = file_get_contents($this->tmpFile);
     $this->assertEquals($expectedOutput, $actualOutput);
@@ -69,11 +72,11 @@ class ExtractCommandTest extends \PHPUnit\Framework\TestCase {
   public function testExecuteWithHeader() {
     chdir($this->getBaseDir());
     $commandTester = $this->createCommandTester(new ExtractCommand());
-    $commandTester->execute(array(
+    $commandTester->execute([
       'command' => self::COMMAND,
-      'files' => array('examples/ex1.php'),
+      'files' => ['examples/ex1.php'],
       '--header' => 'examples/header',
-    ));
+    ]);
     $expectedOutput = file_get_contents('examples/header') . file_get_contents('examples/ex1.pot');
     $this->assertEquals($expectedOutput, $commandTester->getDisplay());
   }
@@ -89,14 +92,15 @@ class ExtractCommandTest extends \PHPUnit\Framework\TestCase {
     fwrite($fh, "examples/ex4.install\n");
     fwrite($fh, "examples/ex4.tpl\n");
     fwrite($fh, "examples/ex4.hlp\n");
-    fwrite($fh, "examples/ex4.module\n"); // note: duplicated in 'files' below
+    // note: duplicated in 'files' below
+    fwrite($fh, "examples/ex4.module\n");
     rewind($fh);
 
     $commandTester = $this->createCommandTester(new ExtractCommand(NULL, $fh));
-    $commandTester->execute(array(
+    $commandTester->execute([
       'command' => self::COMMAND,
-      'files' => array("-", "examples/ex4.module", "examples/ex4.js", "examples/ex4.cmd2", "examples/ex4.txt"),
-    ));
+      'files' => ["-", "examples/ex4.module", "examples/ex4.js", "examples/ex4.cmd2", "examples/ex4.txt"],
+    ]);
     $expectedOutput = file_get_contents("examples/ex4.pot");
     $this->assertEquals($expectedOutput, $commandTester->getDisplay());
 
