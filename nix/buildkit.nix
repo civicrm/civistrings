08687561799aab42079ca4c373fa60ab33c6eeb9
Tim Otten <totten@civicrm.org>
2026-01-27 15:58:53 -0800

    Update buildkit revision

--- nix/buildkit.nix
+++ nix/buildkit.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 ## Get civicrm-buildkit from github.
-## Based on "master" branch circa 2025-12-17 04:09 UTC
+## Based on "master" branch circa 2026-01-27 23:56 UTC
 import (pkgs.fetchzip {
-  url = "https://github.com/civicrm/civicrm-buildkit/archive/e9aa5e95195ac82d6058c6929887ea91917f14e3.tar.gz";
-  sha256 = "1difffv64aam9jkcn62iqg2v6wccr23r29c624s7l045maby5ak9";
+  url = "https://github.com/civicrm/civicrm-buildkit/archive/ad82671e7468491131e6db19440b26b6b35d8d86.tar.gz";
+  sha256 = "0igyhcd18agy90p4h9gmzx0q8rmigab0hxjc4z7yp6kkc663byya";
 })
 
 ## Get a local copy of civicrm-buildkit. (Useful for developing patches.)
